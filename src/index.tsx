949e1f8c34315d468d4fd767d44f457f76b5b848
leesuun <sktjsvy2@naver.com>
2022-07-12 16:56:37 +0900

    animation project

---
+++ src/index.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import App from "./App";
+const rootElement = document.getElementById("root");
+
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+margin: 0;
+padding: 0;
+border: 0;
+font-size: 100%;
+font: inherit;
+vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+line-height: 1;
+}
+menu, ol, ul {
+list-style: none;
+}
+blockquote, q {
+quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+content: '';
+content: none;
+}
+table {
+border-collapse: collapse;
+border-spacing: 0;
+}
+* {
+box-sizing: border-box;
+}
+body{
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
+  line-height: 1.2;
+  background:linear-gradient(135deg,#e09,#d0e);
+}
+
+a{
+    text-decoration: none;
+    color:inherit;
+}
+`;
+
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = createRoot(rootElement); // createRoot(container!) if you use TypeScript
+
+root.render(
+  <React.StrictMode>
+    <App />
+    <GlobalStyle />
+  </React.StrictMode>
+);
